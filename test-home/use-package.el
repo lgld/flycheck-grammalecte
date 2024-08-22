0b3489ae8ba6624b14ab3ad1b0c28cdc59be6631
Étienne Deparis <etienne@depar.is>
2024-08-22 12:25:48 +0200

    refactor: Change use-package indent / use :custom instead of :init setq

--- test-home/use-package.el
+++ test-home/use-package.el
@@ -1,11 +1,12 @@
-(require 'use-package)
+(use-package use-package)
 
 (use-package flycheck-grammalecte
-             :hook (org-mode . flycheck-mode)
-             :init
-             (setq flycheck-grammalecte-report-apos nil
-                   flycheck-grammalecte-report-esp nil
-                   flycheck-grammalecte-report-nbsp nil)
-             :config
-             (grammalecte-download-grammalecte)
-             (flycheck-grammalecte-setup))
+  :hook (org-mode . flycheck-mode)
+  :custom
+  (flycheck-grammalecte-report-apos nil)
+  (flycheck-grammalecte-report-esp nil)
+  (flycheck-grammalecte-report-nbsp nil)
+  :config
+  (setq grammalecte--debug-mode t)
+  (grammalecte-download-grammalecte)
+  (flycheck-grammalecte-setup))
